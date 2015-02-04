7419524f74d0127eae349a3195e25256ca9b87c5
adareau <alexandre.dareau@gmail.com>
2015-02-04 18:41:29 +0100

    fixing bug coming from last commit - issue with Qt API...

--- tests.py
+++ tests.py
@@ -5,15 +5,8 @@ Created on Tue Nov 11 15:18:25 2014
 @author: alex
 """
 
-import pyfit as pf
+from spyderlib.widgets.externalshell.sitecustomize import runfile
 
-a = pf.pyfit_classes.Value(name = 'a', formula = lambda x:0)
-b = pf.pyfit_classes.Value(name = 'b', formula = lambda x:1)
 
-ls = (a,b)
-
-for v in ls:
-    if v.name == 'a':
-        v.formula = lambda x:3
-
+runfile('start.py')
 
